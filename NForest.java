b4cb05d918f26c19e91b2dd2ce95998824c6cd75
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2024-03-22 18:32:15 +0530

    Create NForest.java
    Question Link : https://www.codingninjas.com/studio/problems/n-forest_6570177?leftPanelTabValue=PROBLEM

---
+++ NForest.java
@@ -0,0 +1,12 @@
+// Question link : https://www.codingninjas.com/studio/problems/n-forest_6570177?leftPanelTabValue=PROBLEM
+public class Solution {
+    public static void nForest(int n) {
+        // Write your code here
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
